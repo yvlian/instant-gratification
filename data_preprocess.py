474ad96ffba027cab82ce5135a42f0dee56cd1d6
yvlian <2509123408@qq.com>
2019-06-04 21:08:39 +0800

    1

--- data_preprocess.py
+++ data_preprocess.py
@@ -25,50 +25,50 @@ if sum(df['id'].duplicated()) != 0:
 # df.describe()
 
 id = df.pop('id')
-#4 normalize 1th
-cols_except_lable_col = df.columns.drop(label_col_name)
-df[cols_except_lable_col] = df[cols_except_lable_col].apply(lambda x:normalize(x))
-
-
-corr = df.corr()
-describe = corr[label_col_name].abs().describe()
-corr = corr[corr[label_col_name]!=1]
-items = corr[label_col_name].items()
-corr.pop(label_col_name)
-#去除同label低相关的属性
-for k,v in items:
-    if abs(v) < describe['25%']:
-        df.pop(k)
-#相关度高的属性去重
-highly_correlated = dict()
-for col in corr.columns:
-    index = corr[col][corr[col]>0.9].index
-    index = index.drop(col)
-    highly_correlated[col] = index
-
-for k,v,in highly_correlated.items():
-    if k in df.columns:
-        flag = False
-        for i in v:
-            if i in df.columns:
-                flag = True
-                break
-        if flag:
-            df.pop(k)
-
-#构造新特征 x -> (x^2 -1)/2 (x^3 -1)/3  e^x logx
-cols_except_lable_col = df.columns.drop(label_col_name)
-for col in cols_except_lable_col:
-    df[col + 'X^2'] = df[col].apply(lambda x: (x**2-1)/2)
-    df[col + 'X^3'] = df[col].apply(lambda x: (x**3-1)/3)
-    df[col + 'e^X'] = df[col].apply(lambda x: np.exp(x))
-    df[col + 'logX'] = df[col].apply(lambda x: np.log(np.abs(x)))
-
-#normalize 2th
-cols_except_lable_col = df.columns.drop(label_col_name)
-df[cols_except_lable_col] = df[cols_except_lable_col].apply(lambda x:normalize(x))
-
-#model
+# #4 normalize 1th
+# cols_except_lable_col = df.columns.drop(label_col_name)
+# df[cols_except_lable_col] = df[cols_except_lable_col].apply(lambda x:normalize(x))
+#
+#
+# corr = df.corr()
+# describe = corr[label_col_name].abs().describe()
+# corr = corr[corr[label_col_name]!=1]
+# items = corr[label_col_name].items()
+# corr.pop(label_col_name)
+# #去除同label低相关的属性
+# for k,v in items:
+#     if abs(v) < describe['25%']:
+#         df.pop(k)
+# #相关度高的属性去重
+# highly_correlated = dict()
+# for col in corr.columns:
+#     index = corr[col][corr[col]>0.9].index
+#     index = index.drop(col)
+#     highly_correlated[col] = index
+#
+# for k,v,in highly_correlated.items():
+#     if k in df.columns:
+#         flag = False
+#         for i in v:
+#             if i in df.columns:
+#                 flag = True
+#                 break
+#         if flag:
+#             df.pop(k)
+#
+# #构造新特征 x -> (x^2 -1)/2 (x^3 -1)/3  e^x logx
+# cols_except_lable_col = df.columns.drop(label_col_name)
+# for col in cols_except_lable_col:
+#     df[col + 'X^2'] = df[col].apply(lambda x: (x**2-1)/2)
+#     df[col + 'X^3'] = df[col].apply(lambda x: (x**3-1)/3)
+#     df[col + 'e^X'] = df[col].apply(lambda x: np.exp(x))
+#     df[col + 'logX'] = df[col].apply(lambda x: np.log(np.abs(x)))
+#
+# #normalize 2th
+# cols_except_lable_col = df.columns.drop(label_col_name)
+# df[cols_except_lable_col] = df[cols_except_lable_col].apply(lambda x:normalize(x))
+#
+# #model
 
 label = df.pop(label_col_name)
 X_train, X_test, y_train, y_test = train_test_split(df, label, test_size=0.33, random_state=42)
